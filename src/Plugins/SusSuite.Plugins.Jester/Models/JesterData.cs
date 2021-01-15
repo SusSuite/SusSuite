a6e1a5fb9c25c9a993c66bdff9f6e231c7804852
Gavin Steinhoff <GavinSteinhoff@gmail.com>
2021-01-15 17:27:26 -0600

    removed plugins to seperate repo

--- src/Plugins/SusSuite.Plugins.Jester/Models/JesterData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SusSuite.Plugins.Jester.Models
-{
-    public class JesterData
-    {
-        public int JesterId { get; set; }
-        public bool BeenNotified { get; set; } = false;
-    }
-}
